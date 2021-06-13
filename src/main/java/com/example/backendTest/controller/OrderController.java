501faf619c80b04207a921470f0c9349225bec67
pys-durup <enenfnq@gmail.com>
2021-06-13 19:56:30 +0900

    /order 주문 정보 검증 에러메세지 반환

--- src/main/java/com/example/backendTest/controller/OrderController.java
+++ src/main/java/com/example/backendTest/controller/OrderController.java
@@ -6,6 +6,8 @@ import com.example.backendTest.service.OrderVerification;
 import com.example.backendTest.service.OrderServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +28,7 @@ public class OrderController {
     }
 
     @PostMapping(value = "/order")
-    public String order(@RequestBody HashMap<String, Object> messageBody) {
+    public Object order(@RequestBody HashMap<String, Object> messageBody) {
 
         System.out.println("messageBody = " + messageBody);
 
@@ -36,19 +38,29 @@ public class OrderController {
         // 주문 상품 정보
         ItemsDTO itemsDTO = objectMapper.convertValue(messageBody.get("items"), ItemsDTO.class);
 
-        // 상품 검증 (상품이 있는지, 재고가 0개 이상으로 주문 가능한지)
+        // 상품 검증 - 상품이 있는지
         boolean hasProduct = orderVerification.hasProduct(itemsDTO.getItemType(), itemsDTO.getId());
-        boolean canOrder = orderVerification.canOrder(itemsDTO.getItemType(), itemsDTO.getId());
 
         if (hasProduct == false) {
             // 상품 없음 에러 반환
-            System.out.println("error no item");
+            System.out.println("There is no item");
+            return new ResponseEntity("There is no item", HttpStatus.BAD_REQUEST);
+
         }
+
+        // 상품 검증 - 재고가 0개 이상으로 주문 가능한지
+        boolean canOrder = orderVerification.canOrder(itemsDTO.getItemType(), itemsDTO.getId());
 
         if (canOrder == false) {
             // 상품 재고 0 에러 반환
-            System.out.println("error 0 stock");
+            System.out.println("There is no stock");
+            return new ResponseEntity("There is no stock", HttpStatus.INTERNAL_SERVER_ERROR);
         }
+
+
+        // 주문 정보 저장
+
+
 
         return "restTest";
     }
